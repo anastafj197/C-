e0c9d153bb272dc6f6542cf0c7f11be5a7de17aa
anastafj197 <frankie.anastasia@gmail.com>
2018-09-03 22:28:03 -0400

    Update
    - String concatenation 
    - User input

--- learn.cpp
+++ learn.cpp
@@ -1,10 +1,27 @@
-#Frank Anastasia 
+# Frank Anastasia 
+# Reviewing basics 
 
 include <iostream>
 using namespace std;
 
 int main() {
   int value = 6;
-  string stuff = "Bunch of stuff";
+  string text1 = "Bunch";
+  string text2 = " of stuff";
+  string things = text1 = text2;
+  
+  # Insertion operator (<<)
   cout << value << endl;
+  cout << things << endl; 
+ 
+  # Flush is no new line 
+  cout << "Enter anything" << flush; 
+ 
+  string input; 
+  
+  # Extraction operator (>>) user input 
+  cin >> input; 
+  
+  cout << input << endl; 
+  return 0; 
 }
